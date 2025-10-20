a3f463fb46fa4430313efc47abd28e0e13d6f87f
klabarre <klabarre@dawan.fr>
2025-10-20 15:42:26 +0200

    ajout correction exo filter()

--- 03_tableaux/03_fonctionFilter.js
+++ 03_tableaux/03_fonctionFilter.js
@@ -15,16 +15,30 @@ numbersFilter = numbers.filter((number) => number > 5);
 console.log(numbersFilter);
 
 // 1. Créer un tableau qui contient les nombres pairs ( x % 2 === 0 )
+const numbersPairs = numbers.filter((n) => n % 2 === 0);
+console.log("nombres paires: ", numbersPairs);
 
 // 2.  Faire une fonction 'textFilter(entreeUtilisateur)' qui prend en parametre une chaine de caractères
-//  --> la fonction doit retourner un tableau qui contient uniquement les mots qui contiennent la chaine de caractères passée en param
+// --> la fonction doit trier le tableau 'words' pour ne garder que les mots qui contiennent le texte passé en param
+// const words = ["spray", "elite", "exuberant", "destruction", "present"];
 
-// Pour le tri, on peut utiliser include ou indexOf  ( voir W3 school ou MDN (https://developer.mozilla.org) )
-
-const words = ["spray", "elite", "exuberant", "destruction", "present"];
+// Pour le tri, on peut utiliser includes ou indexOf  ( voir W3 school ou MDN (https://developer.mozilla.org) )
 
 // Par exemple -> console.log(textFilter("es"));
 // --> resultat: ["destruction", "present"]
 
-// Trier le tableau 'words', garder les mots qui contiennent le texte passé en param
-// Pour le tri, on peut utiliser indexOf ou includes
+const textFilter1 = (entreeUtilisateur) => {
+  const result = words.filter((word) => word.includes(entreeUtilisateur));
+  return result;
+};
+
+// equivalent de la solution ci-dessus, avec un syntaxe plus concise
+const textFilter2 = (entreeUtilisateur) =>
+  words.filter((word) => word.includes(entreeUtilisateur));
+
+// Test des fonctions
+const result = textFilter1("es");
+const result2 = textFilter2("es");
+
+console.log("resultat recherche 1 'es': ", result);
+console.log("resultat recherche 2 'es': ", result2);
